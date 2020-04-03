1162128bcdae19af2092f1e88117193c8b9b2365
Marie Tulliez <marie-tulliezhotmail.fr>
2020-04-03 11:57:44 +0200

    Implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,4 +1,46 @@
 
+/**
+ * 
+ * @author Marie
+ *
+ */
+
 public class Utilisateur {
 
-}
+	/*==================================Propriétés=============================================*/
+	
+	private int id;
+	private String nom;
+	
+	
+	/*==================================Constructeur=============================================*/
+	
+	public Utilisateur() {
+		
+	}
+
+
+	/*==================================GETTER SETTER=============================================*/
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	} 
+	
+	
+}//end classe
